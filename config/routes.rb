7d9474592f972496f7383a3e8d4202b90fb8cd3d
bracikaa <mehmed_duhovic@hotmail.com>
2017-10-29 18:24:01 +0100

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :portfolio_posts
+  resources :portfolio_posts, except: [:show]
+  get 'portfolio_post/:id', to: 'portfolio_posts#show', as: 'portfolio_post_show'
   
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
